70c8557452041c5dc8eafd0bd8d34d5b9f4d4348
Pablo Garcia <polteus2001@gmail.com>
2021-04-14 19:14:24 +0200

    reactive single reactive forms in the stepper and custom validation

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import {FormBuilder, FormGroup, AbstractControl, Validators} from '@angular/forms';
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import { FormBuilder, FormGroup, FormArray, Validators, FormControl } from '@angular/forms';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 @Component({
@@ -9,19 +9,18 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./stepper.component.scss']
 })
 export class StepperComponent implements OnInit {
- payForm: FormGroup;
- /** Returns a FormArray with the name 'formArray'. */
- get formArray(): AbstractControl | null { return this.payForm.get('formArray'); }
- //Detecta si el tamaño de pantalla se adecua a la query Handset
- isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
- .pipe(
-   map(result => result.matches),
-   shareReplay()
- );
-//--------
-@Output() showStepper = new EventEmitter<boolean>();
-public congrats: boolean;
-  constructor(private fb: FormBuilder, private breakpointObserver: BreakpointObserver) {}
+  payForm: FormGroup;
+
+  //Detecta si el tamaño de pantalla se adecúa a la query Handset
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+  //--------
+  @Output() showStepper = new EventEmitter<boolean>();
+  public congrats: boolean;
+  constructor(private fb: FormBuilder, private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit() {
     this.congrats = false;
@@ -29,18 +28,32 @@ public congrats: boolean;
     this.payForm = this.fb.group({
       formArray: this.fb.array([
         this.fb.group({
-          nameFormCtrl: ['', Validators.required]
+          nameFormCtrl: ['', [
+            Validators.required,
+            Validators.pattern('[a-zA-Z\u00C0-\u017F\ ]+')
+          ]
+          ]
         }),
         this.fb.group({
           addressFormCtrl: ['', Validators.required]
         }),
         this.fb.group({
-          cardNumFormCtrl: ['', Validators.required]
+          cardNumFormCtrl: [
+            null,
+            [Validators.required,
+            Validators.pattern('^[0-9]{16}$')
+            ]
+          ]
         })
       ])
     });
-  }
 
+  }
+  //Returns a FormArray with the name 'formArray'
+  get formArray() {
+    return this.payForm.get('formArray') as FormArray;
+  }
+  //------
   letChargeSomeMoney() {
     this.congrats = true;
     setTimeout(() => { this.showStepper.emit(false); }, 4000);
